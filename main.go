27910895594692402b9de97abd4ca4a5079a815a
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-02 01:28:33 +0200

    added upload func

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"log"
 	"path"
 	"fmt"
+	"bytes"
+	"strings"
+	"io"
 )
 
 // main func
@@ -18,39 +21,55 @@ func routes() {
 	// init router
 	r := mux.NewRouter()
 	// index route
-	r.HandleFunc("/", HomeHandler)
-	r.HandleFunc("/share", ShareHandler)
-	r.HandleFunc("/download/", DownloadHandler)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/upload", upload)
+	r.HandleFunc("/download/", download)
 
 	// start server on port 1337
 	log.Fatal(http.ListenAndServe(":1337", r))
 }
 
 // serves index file
-func HomeHandler(w http.ResponseWriter, r*http.Request) {
-	p := path.Dir("./public/views/index.html")
+func home(w http.ResponseWriter, r*http.Request) {
+	p := path.Dir("./static/index.html")
 	// set header
 	w.Header().Set("Content-type", "text/html")
 	http.ServeFile(w, r, p)
 }
 
 // get shared files
-func ShareHandler(w http.ResponseWriter, r *http.Request) {
+func upload(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
 		if err := r.ParseForm(); err != nil {
 			fmt.Fprint(w, "ParseForm() err: %v", err)
 			return
 		}
-		log.Println(r.FormValue("name"))
-		http.Redirect(w, r, "/download/", http.StatusMovedPermanently)
+
+		var Buf bytes.Buffer
+		// in your case file would be fileupload
+		file, header, err := r.FormFile("uploadfile")
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		name := strings.Split(header.Filename, ".")
+		fmt.Printf("File name %s\n", name[0])
+		// Copy the file data to buffer
+		io.Copy(&Buf, file)
+		contents := Buf.String()
+		fmt.Println(contents)
+		Buf.Reset()
+		return
+
+		log.Println(r.FormValue("uploadfile"))
+		http.Redirect(w, r, "/", http.StatusMovedPermanently)
 	}
 }
 
-func DownloadHandler(w http.ResponseWriter, r *http.Request) {
-	p := path.Dir("./public/views/share.html")
+func download(w http.ResponseWriter, r *http.Request) {
 	// set header
 	w.Header().Set("Content-type", "text/html")
-	http.ServeFile(w, r, p)
+	http.ServeFile(w, r, "./static/share.html")
 }
 
